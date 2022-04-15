886a6fc6e7ada29b5348446e3e8953f9bc39fe22
Henry-Freck <henry.freck@gmail.com>
2022-04-15 13:56:55 -0400

    three graphs working

--- graphDisplay.php
+++ graphDisplay.php
@@ -25,33 +25,63 @@
     $list_of_wagesAndSalary = getAllWagesAndSalary($user_id);
     $list_of_nonWageIncome = getAllNonWageIncome($user_id);
 
+    //Monthly summary data
     usort($list_of_net, function ($item1, $item2) {
         return (intval($item1['year'].$item1['month'])) > (intval($item1['year'].$item2['month']));
     });
 
-    $dataPoints = array();
-    $dataPoints2 = array();
+    $total_income = array();
+    $total_expenses = array();
+
+    if(empty($list_of_net)){
+        $list_of_net[] = array('month' => 'No Data yet :', 'year' => '', 'rent' => 0, 'bills' =>0, 'transportation' => 0, 'leisure'=>0, 'foodBeverage' => 0, 'wagesAndSalary' => 0, 'NonWageIncome' => 0);
+    }
+
     foreach ($list_of_net as $net):
-        $dataPoints[] = array( "label" => $net['month'].'/'.$net['year'], "y" =>$net['wagesAndSalary'] + $net['NonWageIncome']);
-        $dataPoints2[] = array( "label" => $net['month'].'/'.$net['year'], "y" =>$net['rent'] + $net['bills'] + $net['transportation'] + $net['leisure'] + $net['foodBeverage']);
+        $total_income[] = array( "label" => $net['month'].'/'.$net['year'], "y" =>$net['wagesAndSalary'] + $net['NonWageIncome']);
+        $total_expenses[] = array( "label" => $net['month'].'/'.$net['year'], "y" =>$net['rent'] + $net['bills'] + $net['transportation'] + $net['leisure'] + $net['foodBeverage']);
     endforeach;
 
-    //$net['month'].'/'.$net['year']
-    // $dataPoints = array(
-    //     array("y"=> 41),
-    //     array("y"=> 35),
-    //     array("y"=> 50),
-    //     array("y"=> 45),
-    //     array("y"=> 52),
-    //     array("y"=> 68),
-    //     array("y"=> 38),
-    //     array("y"=> 71),
-    //     array("y"=> 52),
-    //     array("y"=> 60),
-    //     array("y"=> 36),
-    //     array("y"=> 49),
-    //     array("y"=> 41)
-    // );
+    //Expenses data
+    usort($list_of_expenses, function ($item1, $item2) {
+        return (intval($item1['year'].$item1['month'])) > (intval($item1['year'].$item2['month']));
+    });
+
+    $rent = array();
+    $bills = array();
+    $transportation = array();
+    $leisure = array();
+    $food = array();
+
+    if(empty($list_of_expenses)){
+        $list_of_expenses[] = array('month' => 'No Data yet :', 'year' => '', 'rent' => 0, 'bills' =>0, 'transportation' => 0, 'leisure'=>0, 'foodBeverage' => 0);
+    }
+
+    foreach ($list_of_expenses as $expense):
+        $rent[] = array( "label" => $expense['month'].'/'.$expense['year'], "y" => $expense['rent']);
+        $bills[] = array( "label" => $expense['month'].'/'.$expense['year'], "y" => $expense['bills']);
+        $transportation[] = array( "label" => $expense['month'].'/'.$expense['year'], "y" => $expense['transportation']);
+        $leisure[] = array( "label" => $expense['month'].'/'.$expense['year'], "y" => $expense['leisure']);
+        $food[] = array( "label" => $expense['month'].'/'.$expense['year'], "y" => $expense['foodBeverage']);
+    endforeach;
+
+
+    //income data
+    usort($list_of_incomes, function ($item1, $item2) {
+        return (intval($item1['year'].$item1['month'])) > (intval($item1['year'].$item2['month']));
+    });
+
+    $salary = array();
+    $otherIncome = array();
+
+    if(empty($list_of_incomes)){
+        $list_of_incomes[] = array('month' => 'No Data yet :', 'year' => '', 'wagesAndSalary' => 0, 'nonWageIncome' =>0);
+    }
+
+    foreach ($list_of_incomes as $income):
+        $salary[] = array( "label" => $income['month'].'/'.$income['year'], "y" => $income['wagesAndSalary']);
+        $otherIncome[] = array( "label" => $income['month'].'/'.$income['year'], "y" => $income['nonWageIncome']);
+    endforeach;
 
 ?>
 
@@ -60,26 +90,26 @@
 <head>
     <script type="text/javascript">
         window.onload = function () {
-            var chart = new CanvasJS.Chart("chartContainer1", {            
+            var monthlySummary = new CanvasJS.Chart("summaryContainer", {            
             title:{
-                text: "Summary"              
+                text: "Monthly Summary"              
             },
 
             data:[  //array of dataSeries     
             { //dataSeries - first quarter
             /*** Change type "column" to "bar", "area", "line" or "pie"***/        
             type: "column",
-            name: "First Quarter",
+            name: "Income",
             showInLegend: true,
-            dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+            dataPoints: <?php echo json_encode($total_income, JSON_NUMERIC_CHECK); ?>
             },
 
             { //dataSeries - second quarter
 
             type: "column",
-            name: "Second Quarter", 
+            name: "Expenses", 
             showInLegend: true,               
-            dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+            dataPoints: <?php echo json_encode($total_expenses, JSON_NUMERIC_CHECK); ?>
             }
             ],
             /** Set axisY properties here*/
@@ -88,27 +118,47 @@
                 }    
             });
 
-            var chart2 = new CanvasJS.Chart("chartContainer2", {            
+
+            var expenses = new CanvasJS.Chart("expenses", {            
             title:{
-                text: "Not a summary"              
+                text: "Expense Breakdown"              
             },
 
             data:[  //array of dataSeries     
             { //dataSeries - first quarter
             /*** Change type "column" to "bar", "area", "line" or "pie"***/        
             type: "column",
-            name: "First Quarter",
+            name: "Rent",
             showInLegend: true,
-            dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+            dataPoints: <?php echo json_encode($rent, JSON_NUMERIC_CHECK); ?>
             },
 
             { //dataSeries - second quarter
 
             type: "column",
-            name: "Second Quarter", 
+            name: "Bills", 
             showInLegend: true,               
-            dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+            dataPoints: <?php echo json_encode($bills, JSON_NUMERIC_CHECK); ?>
+            },
+            { //dataSeries - second quarter
+            type: "column",
+            name: "Transportation", 
+            showInLegend: true,               
+            dataPoints: <?php echo json_encode($transportation, JSON_NUMERIC_CHECK); ?>
+            },
+            { //dataSeries - second quarter
+            type: "column",
+            name: "Leisure", 
+            showInLegend: true,               
+            dataPoints: <?php echo json_encode($leisure, JSON_NUMERIC_CHECK); ?>
+            },
+            { //dataSeries - second quarter
+            type: "column",
+            name: "Food and Beverage", 
+            showInLegend: true,               
+            dataPoints: <?php echo json_encode($food, JSON_NUMERIC_CHECK); ?>
             }
+
             ],
             /** Set axisY properties here*/
                 axisY:{
@@ -116,15 +166,46 @@
                 }    
             });
 
-            chart.render();
-            chart2.render();
+            var income = new CanvasJS.Chart("income", {            
+            title:{
+                text: "Income Breakdown"              
+            },
+
+            data:[  //array of dataSeries     
+            { //dataSeries - first quarter
+            /*** Change type "column" to "bar", "area", "line" or "pie"***/        
+            type: "column",
+            name: "Salary",
+            showInLegend: true,
+            dataPoints: <?php echo json_encode($salary, JSON_NUMERIC_CHECK); ?>
+            },
+
+            { //dataSeries - second quarter
+
+            type: "column",
+            name: "Other", 
+            showInLegend: true,               
+            dataPoints: <?php echo json_encode($otherIncome, JSON_NUMERIC_CHECK); ?>
+            },
+
+            ],
+            /** Set axisY properties here*/
+                axisY:{
+                prefix: "$",
+                }    
+            });
+
+            monthlySummary.render();
+            expenses.render();
+            income.render();
         }
     </script>
 </head>
 
 <body>
-    <div id="chartContainer1" style="height: 370px; width: 100%;"></div>
-    <div id="chartContainer2" style="height: 370px; width: 100%;"></div>
+    <div id="summaryContainer" style="height: 370px; width: 100%;"></div>
+    <div id="expenses" style="height: 370px; width: 100%;"></div>
+    <div id="income" style="height: 370px; width: 100%;"></div>
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>
